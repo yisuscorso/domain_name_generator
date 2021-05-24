b7732cee61a55406b633db23c7e7e6ed83888ec5
Corso <yisus.corso@gmail.com>
2021-05-24 18:45:02 +0000

    version_one

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["el", "la"];
+let adj = ["maravilloso", "grande"];
+let noun = ["jugada", "venus"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      let newDomain = pronoun[i] + adj[j] + noun[k];
+      let lista = document.getElementById("domain"); //Buscar el elemento con la id dada
+      let newLi = document.createElement("li"); //crear nuevo elemento
+      let contenido = document.createTextNode(newDomain); //crea texto
+      lista.appendChild(newLi); //añade newLi a lista
+      newLi.appendChild(contenido); // añade texto al nuevo newli
+    }
+  }
+}
